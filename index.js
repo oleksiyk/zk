7b00c4eac1240a0ee69a9445ab8772eba16e3291
Oleksiy Krivoshey <oleksiy@luckyteam.co.uk>
2014-05-29 02:04:09 +0300

    Added semicolons

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 "use strict";
 
-exports = module.exports = require('./lib/index')
+exports = module.exports = require('./lib/index');
 
-exports.Error = require('./lib/error')
-exports.Lock = require('./lib/lock')
+exports.Error = require('./lib/error');
+exports.Lock = require('./lib/lock');
